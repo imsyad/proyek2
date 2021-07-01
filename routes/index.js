dd28818eda8fb0f0e3c9dd649d793a21d3cab003
mrafikamil <rafikamilm1@gmail.com>
2021-07-01 18:47:33 +0700

    update serverside

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,45 @@ var fs = require("fs");
 var router = express.Router();
 const tf = require("@tensorflow/tfjs");
 
-function c() {
+function sigmoid(z) {
+  return 1 / (1 + tf.exp(-z));
+}
+function h(x, theta) {
+  const a = tf.tensor2d(x, [1, 13]);
+  const b = tf.tensor2d(theta, [13, 1]);
+  return sigmoid(tf.matMul(a, b));
+}
+
+function predict(x, theta) {
   // This is example of Tensorflow
-  const a = tf.tensor2d([1, 2], [1, 2]);
-  const b = tf.tensor2d([1, 2, 3, 4], [2, 2]);
-  return a.matMul(b);
+  const x_tf = tf.tensor2d(x, [1, 13]);
+  const theta_tf = tf.tensor2d(theta, [13, 1]);
+
+  const h1 = h(x_tf, theta_tf);
+  var res;
+  if (h1 >= 0.5) {
+    res = 1;
+  } else {
+    res = 0;
+  }
+  return [h1, res];
 }
 
 /* GET home page. */
 router
   .get("/", function (req, res, next) {
-    console.log(c());
     res.render("index", { title: "Express" });
   })
   .post("/", function (req, res, next) {
-    fs.readFile("./public/model.json", "utf8", function (err, data) {
+    fs.readFile("./public/training_result.json", "utf8", function (
+      err,
+      data_theta
+    ) {
       if (err) next(err);
       // Do all calculation here
+      var theta = JSON.parse(data_theta);
+      console.log(theta);
+      console.log(theta.values());
       // Assign the result here for sending back to the user
       var hasil = { example: "hello world" };
       res.statusCode = 200;
